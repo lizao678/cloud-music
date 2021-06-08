58c1cca9340e8a596ef20d50dc9f9d4bc53500e8
ppzhang <mzhangyik@.qq.com>
2021-06-08 17:56:02 +0800

    home分支下的第一次提交

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,48 @@
 <template>
   <div id="app">
-    <div class="test"></div>
-    <div>我是段落</div>
+    <Header></Header>
+    <div class="theme">
+      <button @click="myFn('theme')"></button>
+      <button @click="myFn('theme1')"></button>
+      <button @click="myFn('theme2')"></button>
+    </div>
   </div>
 </template>
 
+<script>
+import Header from './components/Header.vue'
+export default {
+  name: 'App',
+  components: {
+    Header
+  },
+  methods: {
+    myFn (data) {
+      document.documentElement.setAttribute('data-theme', data)
+    }
+  }
+}
+</script>
+
 <style lang="scss">
-.test{
+@import "assets/css/variable";
+.theme{
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  button {
   width: 100px;
   height: 100px;
-  background-color: #f00;
+  &:nth-of-type(1) {
+    background: $background-color-theme;
+  }
+  &:nth-of-type(2) {
+    background: $background-color-theme1;
+  }
+  &:nth-of-type(3) {
+    background: $background-color-theme2;
+  }
 }
+}
+
 </style>
